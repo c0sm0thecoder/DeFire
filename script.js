e6f549a6ebf2a25f736156bf26386d8bd0a36cd3
alizadasaleh <helas2003@gmail.com>
2023-10-08 00:24:11 +0400

    Orange Added, Refacturing, Sie of Map

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 // script.js
 
-// Define a custom icon for your marker
-var customIcon = L.icon({
-    iconUrl: 'icon.png',  // Specify the path to your custom icon image
-    iconSize: [32, 32],               // Set the size of the icon
-    iconAnchor: [16, 32],             // Set the anchor point of the icon
-    popupAnchor: [0, -32]             // Set the popup anchor of the icon
-});
-
+class Forest {
+    constructor(lat, lng, prediction, value) { 
+        this.lat = lat
+        this.lng = lng
+        this.prediction = prediction
+        this.value = value
+     }
+  }
 // Initialize the Leaflet map
 var map = L.map('map').setView([38.9637, 35.2433], 6.3); // Default center (New York City)
 
@@ -19,27 +19,31 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 function getMarkerIcon(value) {
     if (value < 50) {
         return L.divIcon({ className: 'custom-marker custom-green' });
-    }  else {
+    }
+    else if(value < 75){
+        return L.divIcon({ className: 'custom-marker custom-orange' });
+    }
+    else {
         return L.divIcon({ className: 'custom-marker custom-red' });
     }
 }
 
-function displayRandomPredictions() {
-    var prediction1 = generateRandomPrediction(38.6742, 29.4059); // Trabzon
-    var prediction2 = generateRandomPrediction(41.1956, 32.6227); // Kahramanmaraş
-    var prediction3 = generateRandomPrediction(37.7819, 30.5665); // Isparta
+function displayPredictions() {
+    var Trabzon = new Forest(38.6742, 29.4059, 50, "High Risk", 80); // Trabzon
+    var Kahramanmarash = new Forest(41.1956, 32.6227, "Low Risk", 30); // Kahramanmarash
+    var Isparta = new Forest(37.7819, 30.5665, "Medium Risk", 56); // Isparta
 
-    var markerIcon1 = getMarkerIcon(prediction1.value);
-    var markerIcon2 = getMarkerIcon(prediction2.value);
-    var markerIcon3 = getMarkerIcon(prediction3.value);
+    var TrabzonIcon = getMarkerIcon(Trabzon.value);
+    var KahramanmarashIcon = getMarkerIcon(Kahramanmarash.value);
+    var IspartaIcon = getMarkerIcon(Isparta.value);
 
-    var marker1 = L.marker([prediction1.lat, prediction1.lng], { icon: markerIcon1 }).addTo(map);
-    var marker2 = L.marker([prediction2.lat, prediction2.lng], { icon: markerIcon2 }).addTo(map);
-    var marker3 = L.marker([prediction3.lat, prediction3.lng], { icon: markerIcon3 }).addTo(map);
+    var TrabzonMarker = L.marker([Trabzon.lat, Trabzon.lng], { icon: TrabzonIcon }).addTo(map);
+    var KahramanmarashMarker = L.marker([Kahramanmarash.lat, Kahramanmarash.lng], { icon: KahramanmarashIcon }).addTo(map);
+    var Isparta3 = L.marker([Isparta.lat, Isparta.lng], { icon: IspartaIcon }).addTo(map);
 
-    marker1.bindPopup(`Region: Trabzon<br>Prediction: ${prediction1.prediction}<br>Value: ${prediction1.value}`).openPopup();
-    marker2.bindPopup(`Region: Kahramanmaraş<br>Prediction: ${prediction2.prediction}<br>Value: ${prediction2.value}`).openPopup();
-    marker3.bindPopup(`Region: Isparta<br>Prediction: ${prediction3.prediction}<br>Value: ${prediction3.value}`).openPopup();
+    TrabzonMarker.bindPopup(`Region: Trabzon<br>Prediction: ${Trabzon.prediction}<br>Value: ${Trabzon.value}`).openPopup();
+    KahramanmarashMarker.bindPopup(`Region: Kahramanmarash<br>Prediction: ${Kahramanmarash.prediction}<br>Value: ${Kahramanmarash.value}`).openPopup();
+    Isparta3.bindPopup(`Region: Isparta<br>Prediction: ${Isparta.prediction}<br>Value: ${Isparta.value}`).openPopup();
 }
 
 var customMarkerStyles = `
@@ -51,8 +55,8 @@ var customMarkerStyles = `
     .custom-green {
         background-color: green;
     }
-    .custom-yellow {
-        background-color: yellow;
+    .custom-orange {
+        background-color: orange;
     }
     .custom-red {
         background-color: red;
@@ -63,13 +67,4 @@ var markerStyle = document.createElement('style');
 markerStyle.innerHTML = customMarkerStyles;
 document.head.appendChild(markerStyle);
 
-function generateRandomPrediction(lat, lng) {
-    var rawValue = Math.random() * 100; // Replace this with your actual value calculation logic
-    var value = rawValue.toFixed(2); // Format the value to two decimal places
-    var prediction = value > 50 ? 'High risk' : 'Low risk';
-
-    return { lat, lng, prediction, value };
-}
-
-// Trigger the random prediction function on page load
-displayRandomPredictions();
+displayPredictions();
